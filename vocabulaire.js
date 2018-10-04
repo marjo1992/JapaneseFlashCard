be8238784d858c573dc978a01f33430504764795
marjo1992 <verelmarjolaine@hotmail.fr>
2018-10-04 08:06:39 +0200

    first commit

---
+++ vocabulaire.js
@@ -0,0 +1,327 @@
+var vocabulaire =
+  [
+    {
+      "info": {
+        "id": 1,
+        "mot": [
+          { "kanji": "日", "furigana": "に"},
+          { "kanji": "本", "furigana": "ほん"},
+          { "kanji": "人", "furigana": "じん"}
+        ],
+        "traduction": "Japonais / Japonaise (nationalité)"
+      }
+    },
+    {
+      "info": {
+        "id": 2,
+        "mot": [
+          { "kanji": "中", "furigana": "ちゅう"},
+          { "kanji": "国", "furigana": "ごく"},
+          { "kanji": "人", "furigana": "じん"}
+        ],
+        "traduction": "Chinois / Chinoise (nationalité)"
+      }
+    },
+    {
+      "info": {
+        "id": 3,
+        "mot": [
+          { "kanji": "イ", "furigana": "い"},
+          { "kanji": "ギ", "furigana": "ぎ"},
+          { "kanji": "リ", "furigana": "り"},
+          { "kanji": "ス", "furigana": "す"},
+          { "kanji": "人", "furigana": "じん"}
+        ],
+        "traduction": "Américain / Américaine (nationalité)"
+      }
+    },
+    {
+      "info": {
+        "id": 4,
+        "mot": [
+          { "kanji": "ア", "furigana": "あ"},
+          { "kanji": "メ", "furigana": "め"},
+          { "kanji": "リ", "furigana": "り"},
+          { "kanji": "カ", "furigana": "か"},
+          { "kanji": "人", "furigana": "じん"}
+        ],
+        "traduction": "Britannique (nationalité)"
+      }
+    },
+    {
+      "info": {
+        "id": 5,
+        "mot": [
+          { "kanji": "韓", "furigana": "かん"},
+          { "kanji": "国", "furigana": "こく"},
+          { "kanji": "人", "furigana": "じん"}
+        ],
+        "traduction": "Coréen / Coréenne (nationalité)"
+      }
+    },
+    {
+      "info": {
+        "id": 6,
+        "mot": [
+          { "kanji": "留", "furigana": "りゅう"},
+          { "kanji": "学", "furigana": "がく"},
+          { "kanji": "生", "furigana": "せい"}
+        ],
+        "traduction": "Etudiant à l’étranger"
+      }
+    },
+    {
+      "info": {
+        "id": 7,
+        "mot": [
+          { "kanji": "会", "furigana": "かい"},
+          { "kanji": "社", "furigana": "しゃ"},
+          { "kanji": "員", "furigana": "いん"}
+        ],
+        "traduction": "Employé"
+      }
+    },
+    {
+      "info": {
+        "id": 8,
+        "mot": [
+          { "kanji": "学", "furigana": "がく"},
+          { "kanji": "生", "furigana": "せい"}
+        ],
+        "traduction": "Etudiant"
+      }
+    },
+    {
+      "info": {
+        "id": 9,
+        "mot": [
+          { "kanji": "医", "furigana": "い"},
+          { "kanji": "者", "furigana": "しゃ"}
+        ],
+        "traduction": "Médecin"
+      }
+    },
+    {
+      "info": {
+        "id": 10,
+        "mot": [
+          { "kanji": "先", "furigana": "せん"},
+          { "kanji": "生", "furigana": "せい"}
+        ],
+        "traduction": "Enseignant, professeur"
+      }
+    },
+    {
+      "info": {
+        "id": 11,
+        "mot": [
+          { "kanji": "男", "furigana": "おとこ"},
+          { "kanji": "の", "furigana": " "},
+          { "kanji": "人", "furigana": "ひと"}
+        ],
+        "traduction": "Homme"
+      }
+    },
+    {
+      "info": {
+        "id": 12,
+        "mot": [
+          { "kanji": "女", "furigana": "おんな"},
+          { "kanji": "の", "furigana": " "},
+          { "kanji": "人", "furigana": "ひと"}
+        ],
+        "traduction": "Femme"
+      }
+    },
+    {
+      "info": {
+        "id": 13,
+        "mot": [
+          { "kanji": "ホ", "furigana": "ほん"}
+        ],
+        "traduction": "Livre"
+      }
+    },
+    {
+      "info": {
+        "id": 14,
+        "mot": [
+          { "kanji": "新", "furigana": "しん"},
+          { "kanji": "聞", "furigana": "ぶん"}
+        ],
+        "traduction": "Journal"
+      }
+    },
+    {
+      "info": {
+        "id": 15,
+        "mot": [
+          { "kanji": "手", "furigana": "て"},
+          { "kanji": "紙", "furigana": "がみ"}
+        ],
+        "traduction": "Lettre"
+      }
+    },
+    {
+      "info": {
+        "id": 16,
+        "mot": [
+          { "kanji": "雑", "furigana": "ざっ"},
+          { "kanji": "誌", "furigana": "し"}
+        ],
+        "traduction": "Magazine"
+      }
+    },
+    {
+      "info": {
+        "id": 17,
+        "mot": [
+          { "kanji": "机", "furigana": "つくえ"}
+        ],
+        "traduction": "Table"
+      }
+    },
+    {
+      "info": {
+        "id": 18,
+        "mot": [
+          { "kanji": "カ", "furigana": "か"},
+          { "kanji": "メ", "furigana": "め"},
+          { "kanji": "ラ", "furigana": "ら"}
+        ],
+        "traduction": "Appareil photo"
+      }
+    },
+    {
+      "info": {
+        "id": 19,
+        "mot": [
+          { "kanji": "テ", "furigana": "か"},
+          { "kanji": "レ", "furigana": "め"},
+          { "kanji": "ビ", "furigana": "ら"}
+        ],
+        "traduction": "Télévision"
+      }
+    },
+    {
+      "info": {
+        "id": 20,
+        "mot": [
+          { "kanji": "椅", "furigana": "い"},
+          { "kanji": "子", "furigana": "す"}
+        ],
+        "traduction": "Chaise"
+      }
+    },
+    {
+      "info": {
+        "id": 21,
+        "mot": [
+          { "kanji": "日", "furigana": "に"},
+          { "kanji": "本", "furigana": "ほん"},
+          { "kanji": "語", "furigana": "ご"}
+        ],
+        "traduction": "Japonais (langue)"
+      }
+    },
+    {
+      "info": {
+        "id": 22,
+        "mot": [
+          { "kanji": "英", "furigana": "えい"},
+          { "kanji": "語", "furigana": "ご"}
+        ],
+        "traduction": "Anglais (langue)"
+      }
+    },
+    {
+      "info": {
+        "id": 23,
+        "mot": [
+          { "kanji": "映", "furigana": "えい"},
+          { "kanji": "画", "furigana": "が"}
+        ],
+        "traduction": "Film"
+      }
+    },
+    {
+      "info": {
+        "id": 24,
+        "mot": [
+          { "kanji": "中", "furigana": "ちゅう"},
+          { "kanji": "国", "furigana": "ごく"},
+          { "kanji": "語", "furigana": "ご"}
+        ],
+        "traduction": "Chinois (langue)"
+      }
+    },
+    {
+      "info": {
+        "id": 25,
+        "mot": [
+          { "kanji": "時", "furigana": "と"},
+          { "kanji": "計", "furigana": "けい"}
+        ],
+        "traduction": "Montre"
+      }
+    },
+    {
+      "info": {
+        "id": 26,
+        "mot": [
+          { "kanji": "傘", "furigana": "かさ"}
+        ],
+        "traduction": "Parapluie"
+      }
+    },
+    {
+      "info": {
+        "id": 27,
+        "mot": [
+          { "kanji": "か", "furigana": " "},
+          { "kanji": "ば", "furigana": " "},
+          { "kanji": "ん", "furigana": " "},
+        ],
+        "traduction": "Sac"
+      }
+    },
+    {
+      "info": {
+        "id": 28,
+        "mot": [
+          { "kanji": "辞", "furigana": "じ"},
+          { "kanji": "書", "furigana": "しょ"}
+        ],
+        "traduction": "Dictionnaire"
+      }
+    },
+    {
+      "info": {
+        "id": 0,
+        "mot": [
+          { "kanji": "", "furigana": ""},
+          { "kanji": "", "furigana": ""},
+          { "kanji": "", "furigana": ""}
+        ],
+        "traduction": ""
+      }
+    },
+    {
+      "info": {
+        "id": 100,
+        "mot": [
+          { "kanji": "犬", "furigana": "いぬ"}
+        ],
+        "traduction": "chien"
+      }
+    },
+    {
+      "info": {
+        "id": 101,
+        "mot": [
+          { "kanji": "猫", "furigana": "ねこ"}
+        ],
+        "traduction": "chat"
+      }
+    },
+  ]
