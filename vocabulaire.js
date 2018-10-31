743dd865a23d0c0eb64b58ac023eb714b3d65d1a
marjolaine_verel <MVerel.prestataire@oui.sncf>
2018-10-31 14:27:11 +0100

    Ajout du vocabulaire jusqu'à la leçon "Temps"

--- vocabulaire.js
+++ vocabulaire.js
@@ -449,75 +449,886 @@ var vocabulaire =
     },
     {
       "info": {
-        "id": 0,
+        "id": 42,
         "mot": [
-          { "kanji": "", "furigana": ""},
-          { "kanji": "", "furigana": ""},
-          { "kanji": "", "furigana": ""}
+          { "kanji": "公​", "furigana": "こう"},
+          { "kanji": "園", "furigana": "えん"}
         ],
-        "traduction": ""
+        "traduction": "Parc"
       }
     },
     {
       "info": {
-        "id": 0,
+        "id": 43,
         "mot": [
-          { "kanji": "", "furigana": ""},
-          { "kanji": "", "furigana": ""},
-          { "kanji": "", "furigana": ""}
+          { "kanji": "花", "furigana": "はな"}
         ],
-        "traduction": ""
+        "traduction": "Fleur"
       }
     },
     {
       "info": {
-        "id": 0,
+        "id": 44,
         "mot": [
-          { "kanji": "", "furigana": ""},
-          { "kanji": "", "furigana": ""},
-          { "kanji": "", "furigana": ""}
+          { "kanji": "石", "furigana": "いし"}
         ],
-        "traduction": ""
+        "traduction": "Pierre"
       }
     },
     {
       "info": {
-        "id": 0,
+        "id": 45,
         "mot": [
-          { "kanji": "", "furigana": ""},
-          { "kanji": "", "furigana": ""},
-          { "kanji": "", "furigana": ""}
+          { "kanji": "庭", "furigana": "にわ"}
         ],
-        "traduction": ""
+        "traduction": "Cour (lieu)"
       }
     },
     {
       "info": {
-        "id": 0,
+        "id": 46,
         "mot": [
-          { "kanji": "", "furigana": ""},
-          { "kanji": "", "furigana": ""},
+          { "kanji": "部​", "furigana": "へ"},
+          { "kanji": "屋", "furigana": "や"}
+        ],
+        "traduction": "Chambre"
+      }
+    },
+    {
+      "info": {
+        "id": 47,
+        "mot": [
+          { "kanji": "犬", "furigana": "いぬ"}
+        ],
+        "traduction": "Chien"
+      }
+    },
+    {
+      "info": {
+        "id": 48,
+        "mot": [
+          { "kanji": "猫", "furigana": "ねこ"}
+        ],
+        "traduction": "Chat"
+      }
+    },
+    {
+      "info": {
+        "id": 49,
+        "mot": [
+          { "kanji": "子​", "furigana": "こ"},
+          { "kanji": "供", "furigana": "ども"}
+        ],
+        "traduction": "Enfant"
+      }
+    },
+    {
+      "info": {
+        "id": 50,
+        "mot": [
+          { "kanji": "居​", "furigana": "い"},
+          { "kanji": "間", "furigana": "ま"}
+        ],
+        "traduction": "Salon"
+      }
+    },
+    {
+      "info": {
+        "id": 51,
+        "mot": [
+          { "kanji": "上", "furigana": "うえ"}
+        ],
+        "traduction": "Dessus"
+      }
+    },
+    {
+      "info": {
+        "id": 52,
+        "mot": [
+          { "kanji": "下", "furigana": "した"}
+        ],
+        "traduction": "Dessous"
+      }
+    },
+    {
+      "info": {
+        "id": 53,
+        "mot": [
+          { "kanji": "後​", "furigana": "うし"},
+          { "kanji": "ろ", "furigana": ""}
+        ],
+        "traduction": "Derrière"
+      }
+    },
+    {
+      "info": {
+        "id": 54,
+        "mot": [
+          { "kanji": "箱", "furigana": "はこ"}
+        ],
+        "traduction": "Boîte"
+      }
+    },
+    {
+      "info": {
+        "id": 55,
+        "mot": [
+          { "kanji": "ベ", "furigana": ""},
+          { "kanji": "ッ", "furigana": ""},
+          { "kanji": "ド", "furigana": ""}
+        ],
+        "traduction": "Lit"
+      }
+    },
+    {
+      "info": {
+        "id": 56,
+        "mot": [
+          { "kanji": "薬", "furigana": "くすり"}
+        ],
+        "traduction": "Médicament"
+      }
+    },
+    {
+      "info": {
+        "id": 57,
+        "mot": [
+          { "kanji": "中", "furigana": "なか"}
+        ],
+        "traduction": "Dans"
+      }
+    },
+    {
+      "info": {
+        "id": 58,
+        "mot": [
+          { "kanji": "隣", "furigana": "となり"}
+        ],
+        "traduction": "Côté"
+      }
+    },
+    {
+      "info": {
+        "id": 59,
+        "mot": [
+          { "kanji": "前", "furigana": "まえ"}
+        ],
+        "traduction": "Dessus"
+      }
+    },
+    {
+      "info": {
+        "id": 60,
+        "mot": [
+          { "kanji": "紅​", "furigana": "こう"},
+          { "kanji": "茶", "furigana": "ちゃ"}
+        ],
+        "traduction": "Thé noir"
+      }
+    },
+    {
+      "info": {
+        "id": 61,
+        "mot": [
+          { "kanji": "グ", "furigana": ""},
+          { "kanji": "ラ", "furigana": ""},
+          { "kanji": "ス", "furigana": ""}
+        ],
+        "traduction": "Verre"
+      }
+    },
+    {
+      "info": {
+        "id": 62,
+        "mot": [
+          { "kanji": "リ", "furigana": ""},
+          { "kanji": "ン", "furigana": ""},
+          { "kanji": "ゴ", "furigana": ""}
+        ],
+        "traduction": "Pomme"
+      }
+    },
+    {
+      "info": {
+        "id": 63,
+        "mot": [
+          { "kanji": "冷​", "furigana": "れい"},
+          { "kanji": "蔵​", "furigana": "ぞう"},
+          { "kanji": "庫", "furigana": "こ"}
+        ],
+        "traduction": "Réfrigérateur"
+      }
+    },
+    {
+      "info": {
+        "id": 64,
+        "mot": [
+          { "kanji": "ビ", "furigana": ""},
+          { "kanji": "ー", "furigana": ""},
+          { "kanji": "ル", "furigana": ""}
+        ],
+        "traduction": "Bière"
+      }
+    },
+    {
+      "info": {
+        "id": 65,
+        "mot": [
+          { "kanji": "茶​", "furigana": "ちゃ"},
+          { "kanji": "碗", "furigana": "わん"}
+        ],
+        "traduction": "Bol"
+      }
+    },
+    {
+      "info": {
+        "id": 66,
+        "mot": [
+          { "kanji": "車", "furigana": "くるま"}
+        ],
+        "traduction": "Voiture"
+      }
+    },
+    {
+      "info": {
+        "id": 67,
+        "mot": [
+          { "kanji": "家", "furigana": "いえ"}
+        ],
+        "traduction": "Maison"
+      }
+    },
+    {
+      "info": {
+        "id": 68,
+        "mot": [
+          { "kanji": "木", "furigana": "き"}
+        ],
+        "traduction": "Arbre"
+      }
+    },
+    {
+      "info": {
+        "id": 69,
+        "mot": [
+          { "kanji": "鳥", "furigana": "とり"}
+        ],
+        "traduction": "Oiseau"
+      }
+    },
+    {
+      "info": {
+        "id": 70,
+        "mot": [
+          { "kanji": "横", "furigana": "よこ"}
+        ],
+        "traduction": "A côté de"
+      }
+    },
+    {
+      "info": {
+        "id": 71,
+        "mot": [
+          { "kanji": "魚", "furigana": "さかな"}
+        ],
+        "traduction": "Poisson"
+      }
+    },
+    {
+      "info": {
+        "id": 72,
+        "mot": [
+          { "kanji": "外", "furigana": "そと"}
+        ],
+        "traduction": "Dehors"
+      }
+    },
+    {
+      "info": {
+        "id": 73,
+        "mot": [
+          { "kanji": "消​​", "furigana": "け"},
+          { "kanji": "し", "furigana": ""},
+          { "kanji": "ゴ​", "furigana": ""},
+          { "kanji": "ム​", "furigana": ""}
+        ],
+        "traduction": "Gomme"
+      }
+    },
+    {
+      "info": {
+        "id": 74,
+        "mot": [
+          { "kanji": "社​", "furigana": "しゃ"},
+          { "kanji": "長", "furigana": "ちょう"}
+        ],
+        "traduction": "Président"
+      }
+    },
+    {
+      "info": {
+        "id": 75,
+        "mot": [
+          { "kanji": "ペ", "furigana": ""},
+          { "kanji": "ン", "furigana": ""},
           { "kanji": "", "furigana": ""}
         ],
-        "traduction": ""
+        "traduction": "Stylo"
+      }
+    },
+    {
+      "info": {
+        "id": 76,
+        "mot": [
+          { "kanji": "財​", "furigana": "さい"},
+          { "kanji": "布", "furigana": "ふ"}
+        ],
+        "traduction": "Portefeuille"
+      }
+    },
+    {
+      "info": {
+        "id": 77,
+        "mot": [
+          { "kanji": "ペ", "furigana": ""},
+          { "kanji": "ッ", "furigana": ""},
+          { "kanji": "ト", "furigana": ""}
+        ],
+        "traduction": "Animal de compagnie"
+      }
+    },
+    {
+      "info": {
+        "id": 78,
+        "mot": [
+          { "kanji": "一", "furigana": "いち"}
+        ],
+        "traduction": "1"
+      }
+    },
+    {
+      "info": {
+        "id": 79,
+        "mot": [
+          { "kanji": "二", "furigana": "に"}
+        ],
+        "traduction": "2"
+      }
+    },
+    {
+      "info": {
+        "id": 80,
+        "mot": [
+          { "kanji": "三", "furigana": "さん"}
+        ],
+        "traduction": "3"
+      }
+    },
+    {
+      "info": {
+        "id": 81,
+        "mot": [
+          { "kanji": "四", "furigana": "よん"}
+        ],
+        "traduction": "4"
+      }
+    },
+    {
+      "info": {
+        "id": 82,
+        "mot": [
+          { "kanji": "五", "furigana": "ご"}
+        ],
+        "traduction": "5"
+      }
+    },
+    {
+      "info": {
+        "id": 83,
+        "mot": [
+          { "kanji": "六", "furigana": "ろく"}
+        ],
+        "traduction": "6"
+      }
+    },
+    {
+      "info": {
+        "id": 84,
+        "mot": [
+          { "kanji": "七", "furigana": "なな"}
+        ],
+        "traduction": "7"
+      }
+    },
+    {
+      "info": {
+        "id": 85,
+        "mot": [
+          { "kanji": "八", "furigana": "はち"}
+        ],
+        "traduction": "8"
+      }
+    },
+    {
+      "info": {
+        "id": 86,
+        "mot": [
+          { "kanji": "九", "furigana": "きゅう"}
+        ],
+        "traduction": "9"
+      }
+    },
+    {
+      "info": {
+        "id": 87,
+        "mot": [
+          { "kanji": "十", "furigana": "じゅう"}
+        ],
+        "traduction": "10"
+      }
+    },
+    {
+      "info": {
+        "id": 88,
+        "mot": [
+          { "kanji": "十​", "furigana": "じゅう"},
+          { "kanji": "一", "furigana": "いち"}
+        ],
+        "traduction": "11"
+      }
+    },
+    {
+      "info": {
+        "id": 89,
+        "mot": [
+          { "kanji": "二​​", "furigana": "に"},
+          { "kanji": "十​", "furigana": "じゅう"},
+          { "kanji": "二", "furigana": "に"}
+        ],
+        "traduction": "22"
+      }
+    },
+    {
+      "info": {
+        "id": 90,
+        "mot": [
+          { "kanji": "三", "furigana": "さん"},
+          { "kanji": "十", "furigana": "じゅう"},
+          { "kanji": "三", "furigana": "さん"}
+        ],
+        "traduction": "33"
+      }
+    },
+    {
+      "info": {
+        "id": 91,
+        "mot": [
+          { "kanji": "四", "furigana": "よん"},
+          { "kanji": "十", "furigana": "じゅう"},
+          { "kanji": "四", "furigana": "よん"}
+        ],
+        "traduction": "44"
+      }
+    },
+    {
+      "info": {
+        "id": 92,
+        "mot": [
+          { "kanji": "五", "furigana": "ご"},
+          { "kanji": "十", "furigana": "じゅう"},
+          { "kanji": "五", "furigana": "ご"}
+        ],
+        "traduction": "55"
+      }
+    },
+    {
+      "info": {
+        "id": 93,
+        "mot": [
+          { "kanji": "六", "furigana": "ろく"},
+          { "kanji": "十", "furigana": "じゅう"},
+          { "kanji": "六", "furigana": "ろく"}
+        ],
+        "traduction": "66"
+      }
+    },
+    {
+      "info": {
+        "id": 94,
+        "mot": [
+          { "kanji": "七", "furigana": "なな"},
+          { "kanji": "十", "furigana": "じゅう"},
+          { "kanji": "七", "furigana": "なな"}
+        ],
+        "traduction": "77"
+      }
+    },
+    {
+      "info": {
+        "id": 95,
+        "mot": [
+          { "kanji": "八", "furigana": "はち"},
+          { "kanji": "十", "furigana": "じゅう"},
+          { "kanji": "八", "furigana": "はち"}
+        ],
+        "traduction": "88"
+      }
+    },
+    {
+      "info": {
+        "id": 96,
+        "mot": [
+          { "kanji": "九", "furigana": "きゅう"},
+          { "kanji": "十", "furigana": "じゅう"},
+          { "kanji": "九", "furigana": "きゅう"}
+        ],
+        "traduction": "99"
+      }
+    },
+    {
+      "info": {
+        "id": 97,
+        "mot": [
+          { "kanji": "1​", "furigana": "いち"},
+          { "kanji": "時", "furigana": "じ"}
+        ],
+        "traduction": "1 heure"
+      }
+    },
+    {
+      "info": {
+        "id": 98,
+        "mot": [
+          { "kanji": "2​", "furigana": "に"},
+          { "kanji": "時", "furigana": "じ"}
+        ],
+        "traduction": "2 heures"
+      }
+    },
+    {
+      "info": {
+        "id": 99,
+        "mot": [
+          { "kanji": "3​", "furigana": "さん"},
+          { "kanji": "時", "furigana": "じ"}
+        ],
+        "traduction": "3 heures"
       }
     },
     {
       "info": {
         "id": 100,
         "mot": [
-          { "kanji": "犬", "furigana": "いぬ"}
+          { "kanji": "4", "furigana": "よ"},
+          { "kanji": "時", "furigana": "じ"}
         ],
-        "traduction": "chien"
+        "traduction": "4 heures"
       }
     },
     {
       "info": {
         "id": 101,
         "mot": [
-          { "kanji": "猫", "furigana": "ねこ"}
+          { "kanji": "5", "furigana": "ご"},
+          { "kanji": "時", "furigana": "じ"}
         ],
-        "traduction": "chat"
+        "traduction": "5 heures"
       }
     },
+    {
+      "info": {
+        "id": 102,
+        "mot": [
+          { "kanji": "6", "furigana": "ろく"},
+          { "kanji": "時", "furigana": "じ"}
+        ],
+        "traduction": "6 heures"
+      }
+    },
+    {
+      "info": {
+        "id": 103,
+        "mot": [
+          { "kanji": "7", "furigana": "しち"},
+          { "kanji": "時", "furigana": "じ"}
+        ],
+        "traduction": "7 heures"
+      }
+    },
+    {
+      "info": {
+        "id": 104,
+        "mot": [
+          { "kanji": "8", "furigana": "はち"},
+          { "kanji": "時", "furigana": "じ"}
+        ],
+        "traduction": "8 heures"
+      }
+    },
+    {
+      "info": {
+        "id": 105,
+        "mot": [
+          { "kanji": "9", "furigana": "く"},
+          { "kanji": "時", "furigana": "じ"}
+        ],
+        "traduction": "9 heures"
+      }
+    },
+    {
+      "info": {
+        "id": 106,
+        "mot": [
+          { "kanji": "10", "furigana": "じゅう"},
+          { "kanji": "時", "furigana": "じ"}
+        ],
+        "traduction": "10 heures"
+      }
+    },
+    {
+      "info": {
+        "id": 107,
+        "mot": [
+          { "kanji": "11", "furigana": "じゅういち"},
+          { "kanji": "時", "furigana": "じ"}
+        ],
+        "traduction": "11 heures"
+      }
+    },
+    {
+      "info": {
+        "id": 108,
+        "mot": [
+          { "kanji": "12", "furigana": "じゅうに"},
+          { "kanji": "時", "furigana": "じ"}
+        ],
+        "traduction": "12 heures"
+      }
+    },
+    {
+      "info": {
+        "id": 109,
+        "mot": [
+          { "kanji": "時", "furigana": "じ"}
+        ],
+        "traduction": "Heure"
+      }
+    },
+    {
+      "info": {
+        "id": 110,
+        "mot": [
+          { "kanji": "分", "furigana": "ふん"}
+        ],
+        "traduction": "Minute"
+      }
+    },
+    {
+      "info": {
+        "id": 110,
+        "mot": [
+          { "kanji": "1", "furigana": "いっ", "attention" : true},
+          { "kanji": "分", "furigana": "ぷん", "attention" : true}
+        ],
+        "traduction": "1 minute"
+      }
+    },
+    {
+      "info": {
+        "id": 111,
+        "mot": [
+          { "kanji": "2", "furigana": "に"},
+          { "kanji": "分", "furigana": "ふん"}
+        ],
+        "traduction": "2 minutes"
+      }
+    },
+    {
+      "info": {
+        "id": 112,
+        "mot": [
+          { "kanji": "3", "furigana": "さん"},
+          { "kanji": "分", "furigana": "ぷん", "attention" : true}
+        ],
+        "traduction": "3 minutes"
+      }
+    },
+    {
+      "info": {
+        "id": 113,
+        "mot": [
+          { "kanji": "4", "furigana": "よん"},
+          { "kanji": "分", "furigana": "ぷん", "attention" : true}
+        ],
+        "traduction": "4 minutes"
+      }
+    },
+    {
+      "info": {
+        "id": 114,
+        "mot": [
+          { "kanji": "5", "furigana": "ご"},
+          { "kanji": "分", "furigana": "ふん"}
+        ],
+        "traduction": "5 minutes"
+      }
+    },
+    {
+      "info": {
+        "id": 115,
+        "mot": [
+          { "kanji": "6", "furigana": "ろ", "attention" : true},
+          { "kanji": "分", "furigana": "ぷん", "attention" : true}
+        ],
+        "traduction": "6 minutes"
+      }
+    },
+    {
+      "info": {
+        "id": 111,
+        "mot": [
+          { "kanji": "7", "furigana": "なな"},
+          { "kanji": "分", "furigana": "ふん"}
+        ],
+        "traduction": "7 minutes"
+      }
+    },
+    {
+      "info": {
+        "id": 116,
+        "mot": [
+          { "kanji": "8", "furigana": "はっ", "attention" : true},
+          { "kanji": "分", "furigana": "ぷん", "attention" : true}
+        ],
+        "traduction": "8 minutes"
+      }
+    },
+    {
+      "info": {
+        "id": 117,
+        "mot": [
+          { "kanji": "9", "furigana": "きゅう"},
+          { "kanji": "分", "furigana": "ふん"}
+        ],
+        "traduction": "9 minutes"
+      }
+    },
+    {
+      "info": {
+        "id": 118,
+        "mot": [
+          { "kanji": "10", "furigana": "じゅっ", "attention" : true},
+          { "kanji": "分", "furigana": "ぷん", "attention" : true}
+        ],
+        "traduction": "10 minutes"
+      }
+    },
+    {
+      "info": {
+        "id": 119,
+        "mot": [
+          { "kanji": "20", "furigana": "にじゅっ", "attention" : true},
+          { "kanji": "分", "furigana": "ぷん", "attention" : true}
+        ],
+        "traduction": "20 minutes"
+      }
+    },
+    {
+      "info": {
+        "id": 122,
+        "mot": [
+          { "kanji": "50", "furigana": "ごじゅっ", "attention" : true},
+          { "kanji": "分", "furigana": "ぷん", "attention" : true}
+        ],
+        "traduction": "50 minutes"
+      }
+    },
+    {
+      "info": {
+        "id": 123,
+        "mot": [
+          { "kanji": "午​", "furigana": "ご"},
+          { "kanji": "後", "furigana": "ご"}
+        ],
+        "traduction": "Après-midi (p.m.)"
+      }
+    },
+    {
+      "info": {
+        "id": 124,
+        "mot": [
+          { "kanji": "午​", "furigana": "ご"},
+          { "kanji": "前", "furigana": "ぜん"}
+        ],
+        "traduction": "Matin (a.m.)"
+      }
+    },
+    {
+      "info": {
+        "id": 125,
+        "mot": [
+          { "kanji": "朝", "furigana": "あさ"}
+        ],
+        "traduction": "Matin"
+      }
+    },
+    {
+      "info": {
+        "id": 126,
+        "mot": [
+          { "kanji": "昼", "furigana": "ひる"}
+        ],
+        "traduction": "Après midi"
+      }
+    },
+    {
+      "info": {
+        "id": 127,
+        "mot": [
+          { "kanji": "夜", "furigana": "よる"}
+        ],
+        "traduction": "Soirée"
+      }
+    },
+    {
+      "info": {
+        "id": 128,
+        "mot": [
+          { "kanji": "今", "furigana": "いま"}
+        ],
+        "traduction": "Maintenant"
+      }
+    },
+    {
+      "info": {
+        "id": 129,
+        "mot": [
+          { "kanji": "1​", "furigana": "いち"},
+          { "kanji": "時", "furigana": "じ"},
+          { "kanji": "半", "furigana": "はん"}
+        ],
+        "traduction": "1 heure et demi"
+      }
+    },
+    {
+      "info": {
+        "id": 130,
+        "mot": [
+          { "kanji": "何​", "furigana": "なん"},
+          { "kanji": "時", "furigana": "じ"}
+        ],
+        "traduction": "Quelle heure ?"
+      }
+    },
+    {
+      "info": {
+        "id": 0,
+        "mot": [
+          { "kanji": "", "furigana": ""},
+          { "kanji": "", "furigana": ""},
+          { "kanji": "", "furigana": ""}
+        ],
+        "traduction": ""
+      }
+    }
   ]
